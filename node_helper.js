1c3afad2250fb200c6e1b80b8d71112eb14217cb
sam detweiler <sdetweil@gmail.com>
2018-11-08 08:26:44 -0600

    add new code for running temps collection process
    run process in background, collect info using handlers, send good content message to module.js thru sendSocketNotification

--- node_helper.js
+++ node_helper.js
@@ -7,7 +7,7 @@
 const NodeHelper = require("node_helper");
 const request = require("pc-stats");
 var lmSensors = require("sensors.js");
-const exec = require("child_process").execFile;
+const [exec, spawn] = require("child_process");
 var executablePath = "D\:\\MagicMirror-tester\\modules\\MMM-PC-Stats\\ConsoleApp2.exe";
 
 module.exports = NodeHelper.create({
@@ -34,13 +34,46 @@ module.exports = NodeHelper.create({
         });
     },
 
-    getTemps: function() {
+    /*getTemps: function() {
         exec("runas /user:Administrator cmd /c", executablePath, (stdout, error)=>{
             if (error) {throw error};
             this.sendSocketNotification("TEMPS_RESULT", stdout);
             console.log(stdout); // for checking
         });
+    },*/
+	    
+    getTemps: function() {
+        // spawn the temp access tool
+        // might want to start it with a parm of the number of seconds to sleep
+        // and let it run full time int he background vs start/stop
+      	var tempsProcess = spawn(executablePath, { detached: false });
+        
+        // Handle messages from process
+        // if error
+        tempsProcess.stderr.on('data', function (data) {
+          var message = data.toString()
+          console.error("ERROR", message.substring(4))
+        })
+        
+        // if good data
+        tempsProcess.stdout.on('data', function (data) {
+          var message = data.toString()
+          // send it on to module
+          this.sendSocketNotification("TEMPS_RESULT", message);
+          console.log("have temps info="+stdout); // for checking
+        })
+	    
+        // if we receive a closed event from the keyword spotter
+        tempsProcess.on('close', function(data) {
+          console.log("temps process closed message="+data)
+        })
+      /*exec(executablePath, (stdout, error)=>{
+            if (error) {console.log("error="+error; throw error};
+            this.sendSocketNotification("TEMPS_RESULT", stdout);
+            console.log(stdout); // for checking
+        }); */
     },
+    
 
     socketNotificationReceived: function(notification, payload) {
         if (notification === "GET_STATS") {
