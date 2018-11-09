98630bbc60de282422442e0a51f7618a0e5460ff
sam detweiler <sdetweil@gmail.com>
2018-11-09 07:48:55 -0600

    add try/catch to help identify exception

--- node_helper.js
+++ node_helper.js
@@ -45,36 +45,40 @@ module.exports = NodeHelper.create({
 
 	getTemps: function() {
     var self=this;
-		// spawn the temp access tool
-		// might want to start it with a parm of the number of seconds to sleep
-		// and let it run full time int he background vs start/stop
-      	var tempsProcess = spawn(executablePath, { detached: false });
+     try{
+        // spawn the temp access tool
+        // might want to start it with a parm of the number of seconds to sleep
+        // and let it run full time in the background vs start/stop
+        var tempsProcess = spawn(executablePath, { detached: false });
 
-		// Handle messages from process
-		// if error
-		tempsProcess.stderr.on("data", function (data) {
-			var message = data.toString()
-			console.error("ERROR", message.substring(4))
-		})
+        // Handle messages from process
+        // if error
+      /*	tempsProcess.stderr.on("data", function (data) {
+          var message = data.toString()
+          console.error("ERROR", message.substring(4))
+        }) */
 
-		// if good data
-		tempsProcess.stdout.on("data", function (data) {
-			var message = data.toString()
-			// send it on to module
-			self.sendSocketNotification("TEMPS_RESULT", message);
-			console.log("have temps info = " + message); // for checking
-		})
+        // if good data
+        tempsProcess.stdout.on("data", function (data) {
+          var message = data.toString()
+          // send it on to module
+          self.sendSocketNotification("TEMPS_RESULT", message);
+          console.log("have temps info = " + message); // for checking
+        })
 
-		// if we receive a closed event from the keyword spotter
-		tempsProcess.on("close", function(data) {
-			console.log("temps process closed message = " + data)
-		})
-
-		/*exec(executablePath, (stdout, error)=>{
-            if (error) {console.log("error="+error; throw error};
-            this.sendSocketNotification("TEMPS_RESULT", stdout);
-            console.log(stdout); // for checking
-        }); */
+        // if we receive a closed event from the keyword spotter
+        tempsProcess.on("close", function(data) {
+          //console.log("temps process closed message = " + data)
+        })
+     }
+   catch(error){     
+      if(error==='EACCES'){
+        consoole.log("Access Denied executing cpu temps pgm, please check and disable your antivirus pgm.")
+      }
+      else {
+        consoole.log("and unexpected Exception occurred, ="+error)
+      }
+   }
 	},
 
 
